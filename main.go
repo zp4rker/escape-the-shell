53a42857f083451ec522f00bbfac6cceba216397
zp4rker <iam@zp4rker.com>
2021-09-23 11:10:24 +1000

    one approach

--- main.go
+++ main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"golang.org/x/term"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("This is working.")
+	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		panic("Encountered an error when initialising a term instance!")
+	}
+	defer term.Restore(int(os.Stdin.Fd()), oldState)
+
+	terminal := term.NewTerminal(os.Stdin, "> ")
+	writeLine(terminal, "Welcome to escape-the-shell!")
+	writeLine(terminal)
+
+	Shell: for {
+		input, err := terminal.ReadLine()
+		if err != nil {
+			panic("Failed to read user input!")
+		}
+
+		args := strings.Fields(input)
+		cmd := strings.ToLower(args[0])
+		args = args[1:]
+
+		switch cmd {
+		case "exit":
+			writeLine(terminal, "Exiting escape-the-shell now...")
+			break Shell
+		default:
+			writeLine(terminal, "Unrecognised command:", cmd)
+		}
+	}
+}
+
+func writeLine(t *term.Terminal, strings ...string) {
+	strings = append(strings, "\n")
+	write(t, strings...)
+}
+
+func write(t *term.Terminal, strings ...string) {
+	var s string
+	if len(strings) > 0 {
+		s = strings[0]
+		for i := 1; i < len(strings); i++ {
+			s += " " + strings[i]
+		}
+	}
+
+	if _, err := t.Write([]byte(s)); err != nil {
+		panic("Failed to write to terminal!")
+	}
 }
